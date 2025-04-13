e37f018f0566805104c2fe82060a132a311309a4
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2025-04-13 20:09:24 +0200

    create module for js and import module quotes.js in to file script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,4 @@
-const quotes = [
-  {
-    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall',
-    author: 'Nelson Mandela'
-  },
-  {
-    quote: 'Life is what happens when you’re busy making other plans',
-    author: 'John Lennon'
-  },
-  {
-    quote: 'The purpose of our lives is to be happy',
-    author: 'Dalai Lama'
-  },
-  {
-    quote: 'Get busy living or get busy dying',
-    author: 'Stephen King'
-  },
-  {
-    quote: 'The biggest adventure you can take is to live the life of your dreams.',
-    author: 'Oprah Winfrey'
-  },
-  {
-    quote: 'The future belongs to those who believe in the beauty of their dreams.',
-    author: 'Eleanor Roosevelt'
-  },
-  {
-    quote: 'The best way to predict your future is to create it.',
-    author: 'Abraham Lincoln'
-  },
-  {
-    quote: 'Believe you can and you`re halfway there.',
-    author: 'Theodore Roosevelt'
-  }
-]
+import quotes from './quotes.js'
 
 const quoteElement = document.getElementById('quote')
 const quoteAuthorElement = document.getElementById('quote-author')
