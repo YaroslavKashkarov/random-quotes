fe998b64e68d5e40f335e0d722f6eb042c04bc9c
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2025-04-13 21:03:07 +0200

    Added button on favorite quotes and write logics for web app

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,35 +1,53 @@
 const quotes = [
   {
     quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall',
-    author: 'Nelson Mandela'
+    author: 'Nelson Mandela',
+    isFavorite: false
   },
   {
     quote: 'Life is what happens when you’re busy making other plans',
-    author: 'John Lennon'
+    author: 'John Lennon',
+    isFavorite: false
   },
   {
     quote: 'The purpose of our lives is to be happy',
-    author: 'Dalai Lama'
+    author: 'Dalai Lama',
+    isFavorite: false
   },
   {
     quote: 'Get busy living or get busy dying',
-    author: 'Stephen King'
+    author: 'Stephen King',
+    isFavorite: false
   },
   {
     quote: 'The biggest adventure you can take is to live the life of your dreams.',
-    author: 'Oprah Winfrey'
+    author: 'Oprah Winfrey',
+    isFavorite: false
   },
   {
     quote: 'The future belongs to those who believe in the beauty of their dreams.',
-    author: 'Eleanor Roosevelt'
+    author: 'Eleanor Roosevelt',
+    isFavorite: false
   },
   {
     quote: 'The best way to predict your future is to create it.',
-    author: 'Abraham Lincoln'
+    author: 'Abraham Lincoln',
+    isFavorite: false
   },
   {
     quote: 'Believe you can and you`re halfway there.',
-    author: 'Theodore Roosevelt'
+    author: 'Theodore Roosevelt',
+    isFavorite: false
+  },
+  {
+    quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+    author: 'Winston Churchill',
+    isFavorite: false
+  },
+  {
+    quote: 'Do something today that your future self will thank you for.',
+    author: 'Unknown',
+    isFavorite: false
   }
 ]
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,32 @@ import quotes from './quotes.js'
 const quoteElement = document.getElementById('quote')
 const quoteAuthorElement = document.getElementById('quote-author')
 const generateBtn = document.getElementById('generate-btn')
+const toggleFavoriteBtn = document.getElementById('toggle-favorite-btn')
 
+let currentQuoteIndex
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length)
-  const randomQuote = quotes[randomIndex]
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length)
+  const randomQuote = quotes[currentQuoteIndex]
   const {quote, author} = randomQuote
-
   // @ts-ignore
   quoteElement.textContent = quote
   // @ts-ignore
   quoteAuthorElement.textContent = author
 }
 
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex]
+  currentQuote.isFavorite = !quotes[currentQuoteIndex].isFavorite
+  // @ts-ignore
+  if(toggleFavoriteBtn.textContent === 'Add to favorites') {
+    // @ts-ignore
+    toggleFavoriteBtn.textContent = 'Remove from favorites'
+  } else {
+    // @ts-ignore
+    toggleFavoriteBtn.textContent = 'Add to favorites'
+  }
+}
+
 // @ts-ignore
 generateBtn.addEventListener('click', generateRandomQuote)
+toggleFavoriteBtn?.addEventListener('click', toggleFavorite)
